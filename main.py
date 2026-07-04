18f4fb36f9988d8ee957115c62329ac2fb275443
rdpbol <jinwaaadil450@gmail.com>
2026-07-04 18:14:14 +0530

    Change text in main.py from Yᴀsʜ to VERRU-GUN-RAKSHIT

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def run_agent(agent_id, cookie, target_id):
                     const box = document.querySelector('div[role="textbox"], [contenteditable="true"]');
                     if (box) {
                         const currentEmoji = emojis[iteration % emojis.length];
-                        const line = "Yᴀsʜ - Hᴀʀɪsʜ - Mᴇᴍᴀx 𝚃𝙼𝙺𝙲 " + currentEmoji + "་༘࿐";
+                        const line = "VERRU-GUN-RAKSHIT 𝚃𝙼𝙺𝙲 " + currentEmoji + "་༘࿐";
                         
                         let text = "";
                         for(let i = 0; i < 10; i++) { text += line + "\\n\\n\\n\\n"; }
